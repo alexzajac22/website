812161e6364391498e3695223cb7962f295e06d6
alexzajac22 <azajac02@gmail.com>
2025-01-25 17:01:24 -0600

    dawd
    daw

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefualt()
+
+    emailjs.sendForm("service_yt1px5u","template_n3w06ak","#contact-form","NjEgegrUq6EGdu7vN ")
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
